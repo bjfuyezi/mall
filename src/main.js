b9716c8b19c1153deb9a093a4dc154cd3e539bcc
yc2574 <1985810807@qq.com>
2024-12-28 20:53:06 +0800

    "homepage"

--- src/main.js
+++ src/main.js
@@ -4,6 +4,8 @@ import router from './router'
 import store from './store'
 import ElementUI from 'element-ui'
 import 'element-ui/lib/theme-chalk/index.css'
+import 'slick-carousel/slick/slick.css';
+import 'slick-carousel/slick/slick-theme.css';
 
 Vue.use(ElementUI)
 
