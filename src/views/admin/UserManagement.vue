7c059f5e7a0d9f90efaaed150179bd1c2bc76033
bjfuyezi <bjfuzph0214@bjfu.edu.cn>
2025-01-09 15:53:38 +0800

    管理员页面-用户管理

--- src/views/admin/UserManagement.vue
+++ src/views/admin/UserManagement.vue
@@ -1,6 +1,7 @@
 <template>
   <div class="user-management">
     <h2>用户管理</h2>
+    <el-button type="primary" @click="showAddUserDialog">添加用户</el-button>
     <el-table :data="users" style="width: 100%">
       <el-table-column prop="id" label="用户ID" width="80"></el-table-column>
       <el-table-column prop="username" label="用户名"></el-table-column>
@@ -12,6 +13,39 @@
         </template>
       </el-table-column>
     </el-table>
+
+    <el-dialog title="添加用户" :visible.sync="addUserDialog">
+      <el-form :model="newUser">
+        <el-form-item label="用户名">
+          <el-input v-model="newUser.username"></el-input>
+        </el-form-item>
+        <el-form-item label="密码">
+          <el-input type="password" v-model="newUser.password"></el-input>
+        </el-form-item>
+        <el-form-item label="邮箱">
+          <el-input v-model="newUser.email"></el-input>
+        </el-form-item>
+      </el-form>
+      <span slot="footer" class="dialog-footer">
+        <el-button @click="addUserDialog = false">取 消</el-button>
+        <el-button type="primary" @click="addUser">确 定</el-button>
+      </span>
+    </el-dialog>
+
+    <el-dialog title="编辑用户" :visible.sync="editUserDialog">
+      <el-form :model="currentUser">
+        <el-form-item label="用户名">
+          <el-input v-model="currentUser.username"></el-input>
+        </el-form-item>
+        <el-form-item label="邮箱">
+          <el-input v-model="currentUser.email"></el-input>
+        </el-form-item>
+      </el-form>
+      <span slot="footer" class="dialog-footer">
+        <el-button @click="editUserDialog = false">取 消</el-button>
+        <el-button type="primary" @click="updateUser">确 定</el-button>
+      </span>
+    </el-dialog>
   </div>
 </template>
 
@@ -21,7 +55,15 @@ import axios from 'axios';
 export default {
   data() {
     return {
-      users: [] // 用户列表
+      users: [], // 用户列表
+      addUserDialog: false,
+      editUserDialog: false,
+      newUser: {
+        username: '',
+        password: '',
+        email: ''
+      },
+      currentUser: {}
     };
   },
   methods: {
@@ -34,14 +76,39 @@ export default {
           console.error('Error fetching users:', error);
         });
     },
+    showAddUserDialog() {
+      this.addUserDialog = true;
+    },
+    addUser() {
+      axios.post('http://localhost:8081/users/add', this.newUser)
+        .then(response => {
+          console.log('Add user response:', response); // 使用 response
+          this.fetchUsers(); // 刷新用户列表
+          this.addUserDialog = false; // 关闭对话框
+        })
+        .catch(error => {
+          console.error('Error adding user:', error);
+        });
+    },
     editUser(user) {
-      console.log('Editing user:', user); // 使用 user
-      
+      this.currentUser = { ...user }; // 复制用户信息
+      this.editUserDialog = true; // 打开编辑对话框
+    },
+    updateUser() {
+      axios.put(`http://localhost:8081/users/edit/${this.currentUser.id}`, this.currentUser)
+        .then(response => {
+          console.log('Update user response:', response); // 使用 response
+          this.fetchUsers(); // 刷新用户列表
+          this.editUserDialog = false; // 关闭对话框
+        })
+        .catch(error => {
+          console.error('Error updating user:', error);
+        });
     },
     deleteUser(userId) {
       axios.delete(`http://localhost:8081/users/delete/${userId}`)
         .then(response => {
-          console.log('Delete response:', response); // 使用 response
+          console.log('Delete user response:', response); // 使用 response
           this.fetchUsers(); // 刷新用户列表
         })
         .catch(error => {
